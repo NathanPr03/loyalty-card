d638e48c6212c236c924e8843fcd06fc3a9e5b72
Nathan03nable <nathan.pringle@n-able.com>
2024-08-06 11:35:34 +0100

    options headers

--- api/all_customers.go
+++ api/all_customers.go
@@ -17,6 +17,14 @@ type BigCustomer struct {
 }
 
 func AllCustomers(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodOptions {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		w.WriteHeader(http.StatusOK)
+		return
+	}
+
 	var dbConnection, err = db.ConnectToDb()
 	if err != nil {
 		http.Error(w, "Error connecting to database: "+err.Error(), http.StatusInternalServerError)

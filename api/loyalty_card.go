800340b148d690222df67036eccbb3a1a5cb38dd
Nathan03nable <nathan.pringle@n-able.com>
2024-08-06 14:37:43 +0100

    no z

--- api/loyalty_card.go
+++ api/loyalty_card.go
@@ -10,7 +10,7 @@ import (
 )
 
 func SetLoyaltyCard(w http.ResponseWriter, request *http.Request) {
-	w.Header().Set("Access-Control-Allow-Origin", "*z")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 	w.Header().Set("Content-Type", "application/json")
